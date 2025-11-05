b28e73c2bac79be8a44260d9001c0289dd16f46b
idontbyte <tanveer.cse69@gmail.com>
2025-11-05 19:58:26 +0600

    a

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  publicDir: 'public',
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    copyPublicDir: true
-  }
+  plugins: [react(), vercel()],
+  // publicDir: 'public',
+  // base: '/',
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets',
+  //   copyPublicDir: true
+  // }
 })
